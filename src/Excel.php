aad69aa5eba23efee57707bad6a01ebae6a5a26d
XY-JX <zhangschooi@qq.com>
2022-03-03 18:12:12 +0800

    Excel相关

--- src/Excel.php
+++ src/Excel.php
@@ -42,7 +42,7 @@ class Excel
         'font' => [
             'name' => '黑体',
             'bold' => true,
-            'size' => 22
+            'size' => 16
         ]
     ];
 
@@ -55,8 +55,8 @@ class Excel
         if (class_exists('PhpOffice\PhpSpreadsheet\Spreadsheet')) {
             self::$spreadsheet = new Spreadsheet();
             self::$worksheet = self:: $spreadsheet->getActiveSheet();
-            self::$worksheet->getDefaultColumnDimension()->setWidth(20);//设置默认宽度
-            self::$worksheet->getDefaultRowDimension()->setRowHeight(30);//设置默认行高
+            self::$worksheet->getDefaultColumnDimension()->setWidth(16);//设置默认宽度
+            self::$worksheet->getDefaultRowDimension()->setRowHeight(20);//设置默认行高
         } else {
             throw new \Exception('not installed : PhpOffice\PhpSpreadsheet , solution : composer require phpoffice/phpspreadsheet');
         }
@@ -82,7 +82,7 @@ class Excel
         }
 
         self::$worksheet->setTitle(self::$title); //设置导出文件名
-        self::$worksheet->getRowDimension(self::$Prow)->setRowHeight(50);//设置第一行行高
+        self::$worksheet->getRowDimension(self::$Prow)->setRowHeight(30);//设置第一行行高
         $titleMerge = reset(self::$width_row) . self::$Prow . ':' . end(self::$width_row) . self::$Prow;
         self::$worksheet->mergeCells($titleMerge);
         self::$worksheet->getStyle($titleMerge)->applyFromArray(self::$style);
